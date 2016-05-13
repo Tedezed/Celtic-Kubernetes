fd3ee7f26b9613dba49f638d03d8c936b51feae5
Juan Manuel Torres <juanmanuel.torres@aventurabinaria.es>
2016-05-13 14:09:39 +0200

    test daemon

--- haproxy_manager/manager.py
+++ haproxy_manager/manager.py
@@ -9,26 +9,27 @@ from deepdiff import DeepDiff
 from manager_tools import *
 from write_template import *
 
-data = get_conf()
-kube_api = data["kube_api"]
-version = data["version"]
-time_sleep = data["sleep"]
+def hap_manager():
+	data = get_conf()
+	kube_api = data["kube_api"]
+	version = data["version"]
+	time_sleep = data["sleep"]
 
-dic_svc_old = {}
-while True:
-	dic_svc_actives = {}
-	get_json_svcs = get_kube_api(kube_api, version, 'services')['items']
-	for svc in get_json_svcs:
-		try:
-			svc_node_port = svc['spec']['ports'][0]['nodePort']
-			svc_name = svc['metadata']['name']
-			if svc_name not in dic_svc_actives:
-				dic_svc_actives[svc_name] = svc_node_port
-		except KeyError:
-			pass
-	ddiff = DeepDiff(dic_svc_actives, dic_svc_old)
- 	if ddiff:
-	 	print "Reload HAProxy"
-	 	write_template_conf()
-	dic_svc_old = dic_svc_actives
-	sleep(time_sleep)
+	dic_svc_old = {}
+	while True:
+		dic_svc_actives = {}
+		get_json_svcs = get_kube_api(kube_api, version, 'services')['items']
+		for svc in get_json_svcs:
+			try:
+				svc_node_port = svc['spec']['ports'][0]['nodePort']
+				svc_name = svc['metadata']['name']
+				if svc_name not in dic_svc_actives:
+					dic_svc_actives[svc_name] = svc_node_port
+			except KeyError:
+				pass
+		ddiff = DeepDiff(dic_svc_actives, dic_svc_old)
+	 	if ddiff:
+		 	print "Reload HAProxy"
+		 	write_template_conf()
+		dic_svc_old = dic_svc_actives
+		sleep(time_sleep)
